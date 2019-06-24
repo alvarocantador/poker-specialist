dc147701f2a13dbd24410f0d3127ffb625276694
Alvaro Inckot <4078365+alvarocantador@users.noreply.github.com>
2019-06-24 20:06:28 -0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='poker-specialist',
+      version='0.1',
+      description='We are the specialists',
+      license='MIT',
+      install_requires=[
+          'pyknow',
+          'lark-parser'
+      ],
+    
+      zip_safe=False)
