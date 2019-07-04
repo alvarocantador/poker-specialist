a65c771a799d02001c462acd9c4371ceefeeeeef
Alvaro Inckot <4078365+alvarocantador@users.noreply.github.com>
2019-07-04 15:37:52 -0300

    added flop semantic rules

--- dsl/semantic.py
+++ dsl/semantic.py
@@ -31,12 +31,45 @@ class PokerSemantic(Transformer):
         self.engine.declare(ReceivedCard(player=token[0], cards=token[1].children))
 
     def pre_flop(self, token):
+       self._street_actions(token, 'PREFLOP')
+    
+    def flop(self, token):
+       self._street_actions(token[2:], 'FLOP')
+
+    def card(self, token):
+        return { 'value': token[0].value, 'suit': token[1].value }
+
+    def fold(self, token):
+        return { 'action': 'fold', 'player': token[0] }
+    
+    def raised(self, token):
+        return { 'action': 'raise', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
+
+    def call(self, token):
+        return { 'action': 'call', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
+    
+    def bet(self, token):
+        return { 'action': 'raise', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
+    
+    def check(self, token):
+        return { 'action': 'check', 'player': token[0] }
+
+    def timeout(self, token):
+        return { 'action': 'timeout', 'player': token[0] }
+    
+    def chat(self, token):
+        pass
+    
+    def uncalled_bet(self, token):
+        pass
+    
+    def _street_actions(self, token, street):
         act = 0
         act_array = []
         action_array = []
         for i, v in  enumerate(token):
             action = v.children[0]
-            if action['action'] == 'timeout':
+            if isinstance(action, NoneType) or action['action'] == 'timeout':
                 continue
             act_array.append(action['player'])
             act = act_array.count(action['player'])
@@ -62,27 +95,4 @@ class PokerSemantic(Transformer):
 
         # Add Fact (just to add in order. Could be part of the prior FOR
         for a in action_array:
-            self.engine.declare(Action(street='PREFLOP', me=False, is_raised=False, player=a['player'], type=a['type'], id=a['id'], act=a['act'], position=a['position']))
-
-    def card(self, token):
-        return { 'value': token[0].value, 'suit': token[1].value }
-
-    def fold(self, token):
-        return { 'action': 'fold', 'player': token[0] }
-    
-    def raised(self, token):
-        return { 'action': 'raise', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
-
-    def call(self, token):
-        return { 'action': 'call', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
-    
-    def bet(self, token):
-        return { 'action': 'raise', 'player': token[0], 'chips': ast.literal_eval(token[1]) }
-    
-    def check(self, token):
-        return { 'action': 'check', 'player': token[0] }
-
-    def timeout(self, token):
-        return { 'action': 'timeout', 'player': token[0] }
-
-    
+            self.engine.declare(Action(street=street, me=False, is_raised=False, player=a['player'], type=a['type'], id=a['id'], act=a['act'], position=a['position']))
