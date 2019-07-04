cbe0c169b4de708f9d38c6db381cdd2e6b7d1f27
Alvaro Inckot <4078365+alvarocantador@users.noreply.github.com>
2019-07-04 01:51:30 -0300

    updated messages

--- gui/main.py
+++ gui/main.py
@@ -6,16 +6,23 @@ class PokerConsole:
     def plot(self, engine=PokerInference()):
         suggestions = []
         me = None
+        hand = None
         for fact in engine.facts.items():
             current_fact = fact[1].as_dict()
             if isinstance(fact[1], Suggestion):
                 suggestions.append(current_fact)
+            if isinstance(fact[1], Hand):
+                hand = current_fact
             if isinstance(fact[1], Player):
                 if 'me' in current_fact: 
                     me = current_fact
 
         # PRINT
+        print("=====================================")
+        print("Hand: {}".format(hand['id']))
         print("Player: {}".format(me['name']))
+        print("Blinds: {}".format(me['bbs']))
+        print("Seat: {}".format(me['seat']))
         print("")
         print("SUGGESTIONS:")
         for suggestion in suggestions:
