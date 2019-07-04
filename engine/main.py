cacf9c777c642d2429b5c3e9701df704350c7e7f
Alvaro Inckot <4078365+alvarocantador@users.noreply.github.com>
2019-07-04 01:46:18 -0300

    print player name and suggestions

--- engine/main.py
+++ engine/main.py
@@ -163,17 +163,14 @@ class PokerInference(KnowledgeEngine):
     # def preflop_group_8_fold(self):
     #     self.declare(Suggestion(street='PREFLOP', 'Desistir da mão'))
 
+    @Rule(AS.player << Player(bbs=None), AS.blind << Blind())
+    def set_player_big_blinds(self, player, blind):
+        self.modify(player, bbs=player['chips']/blind['big'])
 
-    # @Rule(UserInput(is_ok=True),
-    #       AS.player << Player(bbs=None), AS.blind << Blind())
-    # def set_player_big_blinds(self, player, blind):
-    #     self.modify(player, bbs=player['chips']/blind['big'])
-
-    # @Rule(UserInput(is_ok=True),
-    #       AS.player << Player(),
-    #       AS.received_card << ReceivedCard(),
-    #       TEST(lambda player, received_card: player['name'] == received_card['player']))
-    # def set_received_cards(self, player, received_card):
-    #     f_card, s_card = received_card['cards']
-    #     self.modify(player, me=True, cards=received_card['cards'], suited=f_card['suit'] == s_card['suit'])
-    #     self.retract(received_card)
+    @Rule(AS.player << Player(),
+          AS.received_card << ReceivedCard(),
+          TEST(lambda player, received_card: player['name'] == received_card['player']))
+    def set_received_cards(self, player, received_card):
+        f_card, s_card = received_card['cards']
+        self.modify(player, me=True, cards=received_card['cards'], suited=f_card['suit'] == s_card['suit'])
+        self.retract(received_card)
--- gui/main.py
+++ gui/main.py
@@ -1,11 +1,22 @@
 from engine.main import *
 
+
 class PokerConsole:
 
     def plot(self, engine=PokerInference()):
         suggestions = []
-        for i in range(len(engine.facts)):
-            if isinstance(engine.facts[i], Suggestion):
-                suggestions.append(engine.facts[i].as_dict())
-        print(suggestions)
+        me = None
+        for fact in engine.facts.items():
+            current_fact = fact[1].as_dict()
+            if isinstance(fact[1], Suggestion):
+                suggestions.append(current_fact)
+            if isinstance(fact[1], Player):
+                if 'me' in current_fact: 
+                    me = current_fact
 
+        # PRINT
+        print("Player: {}".format(me['name']))
+        print("")
+        print("SUGGESTIONS:")
+        for suggestion in suggestions:
+            print(suggestion['message'])
